92abea3fcfcd60c98e0744cdc9a73c209d0e2512
Darren Norris <doon75@hotmail.com>
2023-12-09 17:41:35 -0300

    organise folders with usethis.

--- data-raw/build_packagge_notes.R
+++ data-raw/build_packagge_notes.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -12,7 +12,9 @@
 #   Install Package:           'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
-
+# Help to build packages...............
+# usethis example https://emilhvitfeldt.com/post/2018-09-02-usethis-workflow-for-package-development/
+# and this: https://r-pkgs.org/data.html
 hello <- function() {
   print("Hello, world!")
 }
