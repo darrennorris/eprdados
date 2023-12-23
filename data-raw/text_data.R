389c369a101ab746d49c367c25e3791964d2d7f3
Darren Norris <doon75@hotmail.com>
2023-12-23 17:09:24 -0300

    raster data used to create virtual species.

--- data-raw/text_data.R
+++ data-raw/text_data.R
@@ -2,5 +2,5 @@ library(usethis)
 
 
 # -------------------------------------------------------------------------
-interview_species_metrics <- read.csv("data-raw/species_interviews.csv")
+interview_species_metrics <- read.csv("data-raw/species_interviews_forest.csv")
 usethis::use_data(interview_species_metrics, overwrite = TRUE)
