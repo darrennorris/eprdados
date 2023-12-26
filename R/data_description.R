73de79bf180b1c2ae3635df4409428a05bccb647
Darren Norris <doon75@hotmail.com>
2023-12-26 11:22:59 -0300

    Add forest cover data.

--- R/data_description.R
+++ R/data_description.R
@@ -37,4 +37,77 @@
 #' table(interview_species_metrics$sp_name)
 "interview_species_metrics"
 
+#' Carnivore species and landscape metrics.
+#'
+#' A subset of three carnivore species abundances and landscape metrics around properties.
+#'
+#' @description
+#' Values for simulated species around rural property locations in Amapá, Brazil.
+#' Includes landscape metrics for MapBiomas forest cover class.
+#' Metrics are calculated in buffers with radius of:
+#'  0.125, 0.25, 0.5, 1, 2, 4, 8 and 16 km.
+#' To reduce influence of satellite image quality, the metric values are
+#' median values from three years:
+#'  study year, year before and year after.
+#'  Values for one metric cpland calculated using package landscapemetrics.
+#'  cpland is the percentage of core area of forest class in relation to the total landscape area.
+#'
+#'  Abundances and presence absence of 3 species simulated using package virtualspecies.
+#'
+#' @format `carnivoros`
+#' is a dataframe, with:
+#' \describe{
+#'   \item{aid}{Property identifier.}
+#'   \item{long}{Longitude. Decimal degrees (EPSG:4326).}
+#'   \item{lat}{Latitude. Decimal degrees (EPSG:4326).}
+#'   \item{lat}{Habitat_WWF. Terrestrial ecoregion. https://doi.org/10.1641/0006-3568(2001)051[0933:TEOTWA]2.0.CO;2}
+#'   \item{metric}{Metric identifier from landscapemetrics.}
+#'   \item{buff_dist_km}{Buffer distance. Radius in km.}
+#'   \item{value_median}{Median value of landscape metric.}
+#'   \item{sp_name}{Identifier for simulated species.}
+#'   \item{sp_ab}{Species abundances.}
+#'   \item{sp_pa}{Species presence absence.}
+#' }
+#' @source <https://doi.org/10.1177/1940082920971747>
+#'
+#' @examples
+#' # check data. 3 species x 106 properties = 318
+#' table(carnivoros$buff_dist_km)
+#' # check data. 106 properties x 8 buffer distances = 848
+#' table(carnivoros$sp_name)
+"carnivoros"
 
+
+#' Forest cover landscape metrics.
+#'
+#' A subset of forest cover landscape metrics around properties.
+#'
+#' @description
+#' Forest cover around rural property locations in Amapá, Brazil.
+#' Includes landscape metrics for MapBiomas forest cover class.
+#' Metrics are calculated in buffers with radius of:
+#'  0.125, 0.25, 0.5, 1, 2, 4, 8 and 16 km.
+#' To reduce influence of satellite image quality, the metric values are
+#' median values from three years:
+#'  study year, year before and year after.
+#'  Values for one metric cpland calculated using package landscapemetrics.
+#'  cpland is the percentage of core area of forest class in relation to the total landscape area.
+#'
+#'
+#' @format `forest_cover_interviews`
+#' is a dataframe, with:
+#' \describe{
+#'   \item{aid}{Property identifier.}
+#'   \item{long}{Longitude. Decimal degrees (EPSG:4326).}
+#'   \item{lat}{Latitude. Decimal degrees (EPSG:4326).}
+#'   \item{lat}{Habitat_WWF. Terrestrial ecoregion. https://doi.org/10.1641/0006-3568(2001)051[0933:TEOTWA]2.0.CO;2}
+#'   \item{metric}{Metric identifier from landscapemetrics.}
+#'   \item{buff_dist_km}{Buffer distance. Radius in km.}
+#'   \item{value_median}{Median value of landscape metric.}
+#' }
+#' @source <https://doi.org/10.1177/1940082920971747>
+#'
+#' @examples
+#' # check data. 106 properties and 8 buffer distances.
+#' table(forest_cover_interviews$buff_dist_km)
+"forest_cover_interviews"
--- data-raw/text_data.R
+++ data-raw/text_data.R
@@ -1,6 +1,21 @@
 library(usethis)
+library(tidyverse)
 
 
 # -------------------------------------------------------------------------
 interview_species_metrics <- read.csv("data-raw/species_interviews_forest.csv")
 usethis::use_data(interview_species_metrics, overwrite = TRUE)
+
+
+# -------------------------------------------------------------------------
+# three carnivore species: sppB = lontra, sppC = cachorro do mato, sppD = onca.
+carnivoros <- interview_species_metrics |>
+  filter(sp_name %in% c("sppB", "sppC", "sppD"))
+usethis::use_data(carnivoros, overwrite = TRUE)
+
+# -------------------------------------------------------------------------
+# forest cover around interviews.
+forest_cover_interviews <- interview_species_metrics |>
+  filter(sp_name == "sppB") |>
+  select(!c(sp_name, sp_ab, sp_pa))
+usethis::use_data(forest_cover_interviews, overwrite = TRUE)
